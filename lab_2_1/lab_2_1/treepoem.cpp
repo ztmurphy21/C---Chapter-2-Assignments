3a8221f56b28a3a7336f96ac7997fc03f8ceb8ea
Zachary Murphy <ztmurphy21@gmail.com>
2017-01-25 21:03:04 -0500

    initial

---
+++ lab_2_1/lab_2_1/treepoem.cpp
@@ -0,0 +1,32 @@
+//Zachary Murphy- CPT180_27
+//This program outputs a nice poem to the user
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+	//output of poem
+	cout << "I thik I shall never see \n";
+	cout << "A poem lovely as a tree. \n\n";
+
+	cout << "A tree whose hungry mouth is prest \n";
+	cout << "Against the earth's sweet flowing breast. \n\n";
+
+	cout << "A tree that looks at God all day, \n";
+	cout << "And lifts her leafy arms to pray;\n\n";
+
+	cout << "A tree that may in Summer wear\n";
+	cout << "A nest of robins in her hair;\n\n";
+
+	cout << "Upon whose bosom snow has lain;\n";
+	cout << "Who intimately lives with rain.\n\n";
+
+	cout << "Poems are made by fools like me, \n";
+	cout << "But only God can make a tree.\n\n\n";
+
+	//ending the program when the user desires by pressing any key.
+	cout << "Press any key to close this window...";
+	_getch(); 
+	return 0;
+
+}
---
+++ lab_2_2/lab_2_2/outline.cpp
@@ -0,0 +1,29 @@
+//Zachary Murphy- CPT180_27
+//This program outputs a outline to the user
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+	//output of poem
+	cout << "I. Introduction \n";
+	cout << "\t A. Thesis \n";
+	cout << "\t B. Roles/Arguments\n";
+
+	cout << "II. Douglas as Lecturer \n";
+	cout << "\t A. History as a slave and acquisition of eduction.\n";
+	cout << "\t\t 1) He experience slavery \n";
+	cout << "\t\t 2) Literacy allowed expression \n";
+	cout << "\t B. Early lecturers, including initial speech before Garrison \n";
+	cout << "\t\t 1) Success of initial speech\n";
+	cout << "\t\t 2) Goals for future speeches\n";
+	cout << "\t C. Effect of lectures on society\n";
+	cout << "\t\t 1) Open eyes \n";
+	cout << "\t\t 2) Encourage activism \n\n\n";
+
+	//ending the program when the user desires by pressing any key.
+	cout << "Press any key to close this window...";
+	_getch();
+	return 0;
+
+}
